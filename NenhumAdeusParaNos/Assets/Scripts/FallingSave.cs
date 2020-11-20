b81c3bf58c0e8a2eb6d3cd54eea1d4251c5ab13f
RafaelRocha <noahjerfersson@gmail.com>
2020-11-20 17:03:33 -0300

    Comida

--- NenhumAdeusParaNos/Assets/Scripts/FallingSave.cs
+++ NenhumAdeusParaNos/Assets/Scripts/FallingSave.cs
@@ -8,11 +8,14 @@ public class FallingSave : MonoBehaviour
     Vector3 lastPosition;
     int layerMask = 0;
     int water = 0;
+    bool emTerra = true;
+    Player player;
 
     private void Start()
     {
         layerMask =~ LayerMask.GetMask("ColisorAgua");
         water = LayerMask.GetMask("ColisorAgua");
+        player = GetComponent<Player>();
     }
 
     private void Update()
@@ -20,10 +23,21 @@ public class FallingSave : MonoBehaviour
         if (Physics.Raycast(transform.position, Vector3.down, 0.5f, layerMask))
         {
             lastPosition = transform.position;
+            if (!emTerra)
+            {
+                emTerra = true;
+                player.CanMove = true;
+            }
         }
         if (Physics.Raycast(transform.position, Vector3.down, 0.5f, water))
         {
             transform.position = lastPosition;
+            emTerra = false;
+        }
+
+        if (!emTerra)
+        {
+            player.CanMove = false;
         }
     }
 
